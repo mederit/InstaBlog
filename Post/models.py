000fcf19906832f53703884befde333e48a7ea6d
Nurba <ndaiyrkanov@gmail.com>
2021-02-24 20:22:23 +0600

    Third commit, made html

--- Post/models.py
+++ Post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    body = models.TextField(db_index=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    slug = models.SlugField(max_length=15, unique=True)
+    date_pub = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-date_pub',)
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
--- Post/urls.py
+++ Post/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('posts/', ),
+    path('posts/', posts_list, name='posts_list'),
 ]
